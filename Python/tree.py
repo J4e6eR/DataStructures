d28ca012150935900da1bbb931b7e740d1d509b0
MitrenKadiwala <20CS021@charusat.edu.in>
2025-06-10 12:31:46 +0530

    Added Breadth-First Search.

--- Python/tree.py
+++ Python/tree.py
@@ -1,39 +1,69 @@
 # TODO: Implement a tree data structure Probably a binary tree data structure
 import random
+from Queue import Queue
 class BSTNode:
   def __init__(self, data):
     self.left = None
     self.right = None
     self.data = data
 
+  def __repr__(self):
+    return f"BST NODE (Heigth: {calcHeight(self)}) -> {self.data}"
 
 # Insertion would be done based on integer value whether its less than or greater than
-def insert(rootNode: BSTNode, data: int, newNode: BSTNode = None):
+def insert(rootNode: BSTNode, data: int):
   """
   newNode added in parameters for enabling recursion
   """
-  if newNode is None:
-    newNode = BSTNode(data)
-  else:
-    # Currently the new node is added just to the root node
-    # TODO: What to do if the height of the tree is more than 1
-    # TODO: Traverse to the appropriate node and then insert, which includes traversal techniques.
-    # TODO: Learn self-balancing binary search trees (namely AVL Trees)
-    if newNode.data <= rootNode.data:
-      rootNode.left = insert(rootNode.left, data, newNode)
-    elif newNode.data > rootNode.data:
-      rootNode.right = insert(rootNode.right, data, newNode)
-    
-    return rootNode
 
-def BFSTraversal(rootNode: BSTNode, debug: bool = False):
-  pass
+  if rootNode is None:
+    rootNode = BSTNode(data)
+  elif data <= rootNode.data:
+    rootNode.left = insert(rootNode.left, data)
+  else:
+    rootNode.right = insert(rootNode.right, data)
+
+  return rootNode
+
+def DEBUG(data): print(data)
+
+def BFSTraversal(rootNode: BSTNode, debug: bool = False, traversalQueue: Queue = None):
+  """
+  BFS traversal has only level-order
+  """
+  if rootNode == None:
+    return
+  if traversalQueue  is None:
+    traversalQueue = Queue(rootNode)
+  else:
+    traversalQueue.append(rootNode)
+  
+  BFSTraversal(rootNode.left, True, traversalQueue)
+  BFSTraversal(rootNode.right, True, traversalQueue)
+  
+  try:
+    bstNode = traversalQueue.pop()
+    if debug:
+      DEBUG(bstNode.data)
+  except AssertionError as e:
+    pass 
 
 def DFSTraversal(rootNode: BSTNode, technique: str):
-  pass
+  match technique:
+    case 'in':
+      # In-order
+      pass
+    case 'pre':
+      # Pre-order
+      pass
+    case 'post':
+      # Post order
+      pass
+    case _:
+      print("No known DFS traversals.")
 
-# Trsversing the entire tree
-def traversal(rootNode: BSTNode, traversalTechnique: str = "bfs"):
+# Traversing the entire tree
+def traversal(rootNode: BSTNode, traversalTechnique: str = "bfs", debug: bool = False):
   """
   As this is a non-linear data structure, it requires special traversal techniques like
   > Breadth First search.
@@ -43,15 +73,38 @@ def traversal(rootNode: BSTNode, traversalTechnique: str = "bfs"):
     > pre-order  ->  <root> <left subtree> <right subtree>
     > post-order ->  <left subtree> <right subtree> <root>
   """
-  
+  match traversalTechnique:
+    case 'bfs':
+      BFSTraversal(rootNode, debug, None)
+    case 'dfs':
+      DFSTraversal(rootNode, 'in')
+    case _:
+      print("No known way to traverse.")
 
 
-  pass
+def calcHeight(rootNode: BSTNode):
+  """
+  <left node> <parent node> <right node> 
+  parent node is the root node
 
+  """
+
+  # Base condition -> When we reach leaf node
+  if rootNode.left == None or rootNode.right == None:
+    return 0
+
+
+  leftHeight = calcHeight(rootNode.left)
+  rightHeight = calcHeight(rootNode.right)
+
+  return max(leftHeight, rightHeight) + 1
 
 if __name__ == "__main__":
-  tree = BSTNode(5)
+  tree = BSTNode(567)
   for i in range(0, 100):
     number = random.randint(1, 100)
     insert(tree, number)
     print(f"Inserted {number}")
+
+  # BFSTraversal(tree, True, None)
+  BFSTraversal(tree, False, None)
