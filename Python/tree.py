6d0625a418951ccd3e94eb9597a846575c3200f3
MitrenKadiwala <20CS021@charusat.edu.in>
2025-06-10 13:26:34 +0530

    Depth first search done. but still might need some modifications.

--- Python/tree.py
+++ Python/tree.py
@@ -27,43 +27,10 @@ def insert(rootNode: BSTNode, data: int):
 
 def DEBUG(data): print(data)
 
-def BFSTraversal(rootNode: BSTNode, debug: bool = False, traversalQueue: Queue = None):
-  """
-  BFS traversal has only level-order
-  """
-  if rootNode == None:
-    return
-  if traversalQueue  is None:
-    traversalQueue = Queue(rootNode)
-  else:
-    traversalQueue.append(rootNode)
-  
-  BFSTraversal(rootNode.left, True, traversalQueue)
-  BFSTraversal(rootNode.right, True, traversalQueue)
-  
-  try:
-    bstNode = traversalQueue.pop()
-    if debug:
-      DEBUG(bstNode.data)
-  except AssertionError as e:
-    pass 
 
-def DFSTraversal(rootNode: BSTNode, technique: str):
-  match technique:
-    case 'in':
-      # In-order
-      pass
-    case 'pre':
-      # Pre-order
-      pass
-    case 'post':
-      # Post order
-      pass
-    case _:
-      print("No known DFS traversals.")
 
 # Traversing the entire tree
-def traversal(rootNode: BSTNode, traversalTechnique: str = "bfs", debug: bool = False):
+def traversal(rootNode: BSTNode, traversalTechnique: str = "bfs", technique: str = 'level', debug: bool = False):
   """
   As this is a non-linear data structure, it requires special traversal techniques like
   > Breadth First search.
@@ -73,11 +40,79 @@ def traversal(rootNode: BSTNode, traversalTechnique: str = "bfs", debug: bool = 
     > pre-order  ->  <root> <left subtree> <right subtree>
     > post-order ->  <left subtree> <right subtree> <root>
   """
+
+  def _BFSTraversal(rootNode: BSTNode, debug: bool = False, traversalQueue: Queue = None):
+    """
+    BFS traversal has only level-order
+    """
+    if rootNode == None:
+      return
+    
+    if traversalQueue is None:
+      traversalQueue = Queue(rootNode)
+    
+    if traversalQueue is not None:
+      # Prevents adding the root node twice in the queue, especially while creating it.
+      traversalQueue.append(rootNode)
+    
+    _BFSTraversal(rootNode.left, True, traversalQueue)
+    _BFSTraversal(rootNode.right, True, traversalQueue)
+    
+    try:
+      bstNode = traversalQueue.pop()
+      if debug:
+        DEBUG(bstNode.data)
+    except AssertionError as e:
+      pass 
+    
+  def _DFSTraversal(rootNode: BSTNode, technique: str = 'pre', debug: bool = False):
+
+    # in-order   ->  <left subtree> <root> <right subtree>
+    def _inorder(rootNode: BSTNode, debug: bool = False):
+      if rootNode == None:
+        return
+      _inorder(rootNode.left, debug)
+      DEBUG(rootNode)
+      _inorder(rootNode.right, debug)
+
+    # pre-order  ->  <root> <left subtree> <right subtree>
+    def _preorder(rootNode: BSTNode, debug: bool  = False):
+      if rootNode == None:
+        return
+      DEBUG(rootNode)
+      _preorder(rootNode.left, debug)
+      _preorder(rootNode.right)
+
+    # post-order ->  <left subtree> <right subtree> <root>
+    def _postorder(rootNode: BSTNode, debug: bool = False):
+      if rootNode == None:
+        return
+      _postorder(rootNode.left, debug)
+      _postorder(rootNode.right, debug)
+      DEBUG(rootNode)
+
+    match technique:
+      case 'in':
+        # In-order
+        _inorder(rootNode, debug)
+        pass
+      case 'pre':
+        # Pre-order
+        _preorder(rootNode, debug)
+        pass
+      case 'post':
+        # Post order
+        _postorder(rootNode, debug)
+        pass
+      case _:
+        print("No known DFS traversals.")
+  
+  
   match traversalTechnique:
     case 'bfs':
-      BFSTraversal(rootNode, debug, None)
+      _BFSTraversal(rootNode, debug, None)
     case 'dfs':
-      DFSTraversal(rootNode, 'in')
+      _DFSTraversal(rootNode, technique)
     case _:
       print("No known way to traverse.")
 
@@ -100,11 +135,11 @@ def calcHeight(rootNode: BSTNode):
   return max(leftHeight, rightHeight) + 1
 
 if __name__ == "__main__":
-  tree = BSTNode(567)
+  tree = BSTNode(10)
   for i in range(0, 100):
     number = random.randint(1, 100)
     insert(tree, number)
     print(f"Inserted {number}")
 
   # BFSTraversal(tree, True, None)
-  BFSTraversal(tree, False, None)
+  traversal(tree, 'dfs', 'in', True)
